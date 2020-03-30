a12405fdf14808e63db247959931f9a4b85fb3c5
Neri-Laredo <nerila1310@gmail.com>
2020-03-30 16:47:14 -0600

    Creamos el State y Leemos lo que el usuario ingreso

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Formulario = () => {
+
+    //state del formulario
+    const [busqueda, guardarBusqueda] = useState({
+        ciudad: '',
+        pais: ''
+    });
+
+    //extraer ciudad y pais
+    const {ciudad, pais} = busqueda;
+
+    //funcion que coloca los elementos en el state
+    const handleChange = e => {
+
+        //actualizar el state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        });
+    }   
+
     return ( 
         <form>
 
             <div className='input-field col s12'> 
-                <input type='text' name='ciudad' id='ciudad'/>
+                <input type='text' name='ciudad' id='ciudad' value={ciudad} onChange={handleChange}/>
                 <label htmlFor='ciudad'>Ciudad: </label>
             </div>
 
             <div className='input-field col s12'>
-                <select name='pais' id='pais'>
+                <select name='pais' id='pais' value={pais} onChange={handleChange}>
                     <option value=''> --Seleccione un País-- </option>
                     <option value="US">Estados Unidos</option>
                     <option value="MX">México</option>
