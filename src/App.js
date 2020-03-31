1df936c85a6464119a532a2247c36c13e219f6eb
Neri-Laredo <nerila1310@gmail.com>
2020-03-30 19:06:14 -0600

    Verificamos resultados de la consulta

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
 
@@ -13,6 +14,8 @@ function App() {
     const [consultar, guardarConsultar] = useState(false);
 
     const [resultado, guardarResultado] = useState({});
+
+    const [error, guardarError] = useState(false);
 
     const {ciudad, pais} = busqueda;
 
@@ -29,12 +32,27 @@ function App() {
 
                 guardarResultado(resultado);
                 guardarConsultar(false);
+            
+                //detecta si hubo resultados correctos en la consola
+                if(resultado.cod ==="404"){
+                    guardarError(true);
+                }else{
+                    guardarError(false);
+                }
+
             }
         }
 
         consultarAPI();
     }, [consultar]);
 
+
+    let componente;
+    if(error){
+        componente = <Error mensaje="No hay resultados" />
+    }else{
+        componente = <Clima resultado={resultado}/>
+    }
 
     return (
         <Fragment>
@@ -54,9 +72,7 @@ function App() {
                             />
                         </div>
                         <div className='col m6 s12'>
-                            <Clima 
-                                resultado={resultado}
-                            />
+                            {componente}
                         </div>
                     </div>
                 </div>
